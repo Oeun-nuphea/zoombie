22d9715093563f1d1db1d82df6410b7244efd767
OUEN NUPHEA <nupheaoeun@gmail.com>
2026-04-10 09:22:36 +0700

    update on damage

--- src/router/index.js
+++ src/router/index.js
@@ -10,11 +10,6 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/presentation',
-      name: 'presentation',
-      component: () => import('../views/PresentationView.vue'),
-    },
-    {
       path: '/game',
       name: 'game',
       meta: {
